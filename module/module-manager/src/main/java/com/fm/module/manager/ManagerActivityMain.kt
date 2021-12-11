bb8bf1479615485e4bacfcd59eb0fbb379759b25
vansingluo <vansingluo@tencent.com>
2021-12-11 22:14:11 +0800

    init Component

---
+++ module/module-manager/src/main/java/com/fm/module/manager/ManagerActivityMain.kt
@@ -0,0 +1,11 @@
+package com.fm.module.manager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManagerActivityMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.manager_activity_main)
+    }
+}
---
+++ module/module-record/src/main/java/com/fm/module/record/RecordActivityMain.kt
@@ -0,0 +1,11 @@
+package com.fm.module.record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecordActivityMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.record_activity_main)
+    }
+}
